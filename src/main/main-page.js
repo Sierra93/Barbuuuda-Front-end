b1376b306a9ca65581470027b06d64c4433de61f
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-12-22 00:28:58 +0300

    Was added images for main page.

--- src/main/main-page.js
+++ src/main/main-page.js
@@ -1,8 +1,55 @@
-import Main from '../components/main.vue'
+import Main from '../components/main.vue';
+import NavMenu from '../components/nav-menu.vue';
+// import $ from "jquery";
 
 export default {
     name: 'main-page',
     components: {
-        Main
+        Main,
+        NavMenu
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        // onChangeTab(evt, type) {
+        //     var i, tabcontent, tablinks;
+        
+        //     if (type == "login") {
+        //       $(".tab-role").removeClass("role-show");
+        //       $(".tab-role").addClass("role-hide");
+        //       $(".register").removeClass("selected-role");
+        //       $(".tab-role").addClass("not-selected-role");
+        //     }
+        
+        //     if (type == "register") {
+        //       $(".tab-role").removeClass("role-hide");
+        //       $(".tab-role").addClass("role-show");
+        //       $(".register").addClass("selected-role");
+        //     }
+        
+        //     // Get all elements with class="tabcontent" and hide them
+        //     tabcontent = document.getElementsByClassName("tabcontent");
+        
+        //     for (i = 0; i < tabcontent.length; i++) {
+        //       tabcontent[i].style.display = "none";
+        //     }
+        
+        //     // Get all elements with class="tablinks" and remove the class "active"
+        //     tablinks = document.getElementsByClassName("tablinks");
+        
+        //     for (i = 0; i < tablinks.length; i++) {
+        //       tablinks[i].className = tablinks[i].className.replace(" active", "");
+        //     }
+        
+        //     // Show the current tab, and add an "active" class to the button that opened the tab
+        //     document.getElementById(type).style.display = "block";
+        //     evt.currentTarget.className += " active";
+        //   }
     }
 }
