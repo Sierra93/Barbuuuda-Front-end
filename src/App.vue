1e7711b184109567e56a1cedcac0e2f0bf07cbfc
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-12-21 20:31:32 +0300

    Was added main page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,3 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template src="./main/main-page.html"></template>
+<script src="./main/main-page.js"></script> 
+<style src="./main/main-page.css"></style>
