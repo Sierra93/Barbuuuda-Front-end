41697c8693cf764862fe2b4bf16e5f49c642bb21
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-12-23 18:24:39 +0300

    Was added footer.

---
+++ src/footer/footer.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// eslint-disable-next-line no-unused-vars
+
+// import Footer from '../components/footer.vue';
+// import $ from "jquery";
+// import axios from 'axios';
+
+// export default {
+//     name: 'app-footer',
+//     components: {
+//         Footer
+//     },
+//     data() { },
+//     created() { },
+//     methods: { }
+// }
+import Footer from '../components/footer.vue';
+// import $ from "jquery";
+
+export default {
+    name: 'app-footer',
+    components: {
+        Footer
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        
+    }
+}
---
+++ src/nav-menu/nav-menu.js
@@ -0,0 +1,20 @@
+import NavMenu from '../components/nav-menu.vue';
+// import $ from "jquery";
+
+export default {
+    name: 'nav-menu',
+    components: {
+        NavMenu
+    },
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    methods: {
+        
+    }
+}
