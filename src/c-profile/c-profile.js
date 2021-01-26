93fbe9974913fc622285892b813d402271222b62
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2021-01-26 19:09:28 +0300

    [#bh0v28] Was loading profile data.

--- src/c-profile/c-profile.js
+++ src/c-profile/c-profile.js
@@ -13,14 +13,29 @@ export default {
     },
     props: ['oData'],
     created() {
-        
+        this._loadingProfile();
     },
     data() {
         return {
-            
+            aProfileData: []
         }
     },    
     methods: {
-       
+        // Функция загружает всю информацию профиля.
+        _loadingProfile() {
+            let oData = this.$parent.oData;
+            let userId = +localStorage["userId"];
+            let sUrl = oData.urlApi.concat("/user/profile?userId=".concat(userId));
+
+            axios.post(sUrl)
+                .then((response) => {
+                    this.aProfileData .push(response.data);
+                    console.log("Данные профиля", this.aProfileData);
+                })
+
+                .catch((XMLHttpRequest) => {
+                    throw new Error(XMLHttpRequest.response.data);
+                });
+        }
     }
 }
