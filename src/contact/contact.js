cc14e9509bafb98e0d46dcafa49232124771d9e2
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2021-05-21 00:13:12 +0300

    [bar-k1bx2r] Добавил метод для выгрузки контактной информации.

---
+++ src/contact/contact.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+// eslint-disable-next-line no-unused-vars
+
+import Vue from "vue";
+import NavMenu from '../components/nav-menu.vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import $ from "jquery";
+
+$(function () {    
+    __VUE_HOT_MAP__.refreshToken();
+});
+
+export default {
+    name: 'contact',
+    components: {
+        NavMenu,
+        VueRouter
+    },
+    created() {
+       this._loadContacts();
+    },
+    data() {
+        return {
+            oContact: {}
+         }
+    },    
+    props: ["oData"],
+    methods: {       
+        // Функция получит контактную информацию.
+        _loadContacts() {
+            let sUrl = this.oData.urlApi.concat("/main/contacts");
+
+            try {
+                axios.post(sUrl)
+                    .then((response) => {
+                        this.oContact = response.data;  
+                        console.log("Контакты", this.oContact);                      
+                    })
+
+                    .catch((XMLHttpRequest) => {
+                        throw new Error(XMLHttpRequest.response.data);
+                    });
+            } 
+            
+            catch (ex) {
+                throw new Error(ex);
+            }
+        }
+    }
+}
